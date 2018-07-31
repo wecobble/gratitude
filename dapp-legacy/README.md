f9bdb0672b49f92fe2101a683ae19f4c130f212e
Philip Arthur Moore <philip@wecobble.com>
2018-07-31 16:51:41 +0700

    Remove dapp front end from this branch.

--- dapp-legacy/README.md
+++
@@ -1,2 +0,0 @@
-# nOS
-sourcetree
--- dapp-legacy/src/js/app.js
+++
@@ -1,6 +0,0 @@
-window.$ = window.jQuery = require('jquery');
-
-// if bootstrap's javascript features required
-// require('bootstrap');
-
-console.log('Hey, it\'s me');
