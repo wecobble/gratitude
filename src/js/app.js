c3d54d8fe6142f08865a033480ba8ce03ef2f324
Al Amin Khan <amnkhan.me@gmail.com>
2018-08-11 19:08:03 +0600

    Fix dApp Responsiveness.
    * Responisve issue fixed
    
    The issue mentioned by @phillip is now fixed.
    
    * Deleted app.js laravel mix auto generated codes
    
    Laravel mix was auto generating some javascript codes. Those were deleted from app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,4 @@ window.$ = window.jQuery = require('jquery');
 // if bootstrap's javascript features required
 // require('bootstrap');
 
-console.log('Hey, it\'s me');
+// console.log('Hey, it\'s me');
